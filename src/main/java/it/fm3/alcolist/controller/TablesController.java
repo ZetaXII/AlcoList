f465e3268443a7e05336c454b63d8dc2bd3522d9
ZetaXII <114579031+ZetaXII@users.noreply.github.com>
2022-11-30 13:21:57 +0100

    Aggiuo il @CrossOrigin ai controller

--- src/main/java/it/fm3/alcolist/controller/TablesController.java
+++ src/main/java/it/fm3/alcolist/controller/TablesController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import it.fm3.alcolist.service.TablesServiceI;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("manage-tables")
 public class TablesController {
 	@Autowired
--- src/main/java/it/fm3/alcolist/controller/UserAccountController.java
+++ src/main/java/it/fm3/alcolist/controller/UserAccountController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import it.fm3.alcolist.DTO.UserAccountDTO;
 import it.fm3.alcolist.service.UserAccountServiceI;
 
 @RestController
+@CrossOrigin
 @RequestMapping("manage-users")
 public class UserAccountController {
 	
