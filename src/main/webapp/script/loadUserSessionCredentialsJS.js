75cd3a14f4053bbbff43c6f10001767ad4725b53
m.baldi <m.baldi@datagrafservizi.it>
2023-01-27 13:45:37 +0100

    cancellati file raddoppiati

--- src/main/webapp/script/loadUserSessionCredentialsJS.js
+++
@@ -1,7 +0,0 @@
-surname = sessionStorage.getItem("surname");
-mainRole = sessionStorage.getItem("mainRole");
-roleList = sessionStorage.getItem("roleList");
-name = sessionStorage.getItem("name");
-uuid = sessionStorage.getItem("uuid");
-email = sessionStorage.getItem("email");
-
