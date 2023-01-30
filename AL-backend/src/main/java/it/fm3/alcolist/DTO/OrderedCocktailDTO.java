0d5f2d2f45c5362a5d24799e00361513ac3b03f5
m.baldi <m.baldi@datagrafservizi.it>
2023-01-30 17:28:27 +0100

    ordination testing

--- AL-backend/src/main/java/it/fm3/alcolist/DTO/OrderedCocktailDTO.java
+++ AL-backend/src/main/java/it/fm3/alcolist/DTO/OrderedCocktailDTO.java
@@ -4,5 +4,13 @@ public class OrderedCocktailDTO {
 	public String cocktailUuid;
 	public String ordinationUuid;
 	//public int quantity;
+	public OrderedCocktailDTO(String cocktailUuid, String ordinationUuid) {
+		super();
+		this.cocktailUuid = cocktailUuid;
+		this.ordinationUuid = ordinationUuid;
+	}
 
+	public OrderedCocktailDTO() {
+		super();
+	}
 }
--- AL-backend/src/test/java/it/fm3/alcolist/test/OrdinationControllerTest.java
+++ AL-backend/src/test/java/it/fm3/alcolist/test/OrdinationControllerTest.java
@@ -1,7 +1,10 @@
 package it.fm3.alcolist.test;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,15 +21,24 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import it.fm3.alcolist.DTO.OrderedCocktailDTO;
 import it.fm3.alcolist.DTO.OrdinationDTO;
+import it.fm3.alcolist.entity.Cocktail;
+import it.fm3.alcolist.entity.OrderedCocktail;
 import it.fm3.alcolist.entity.Ordination;
 import it.fm3.alcolist.entity.Role;
 import it.fm3.alcolist.entity.Tables;
 import it.fm3.alcolist.entity.UserAccount;
 import it.fm3.alcolist.service.OrdinationServiceI;
+import it.fm3.alcolist.utils.OrdinationStatusEnum;
 
 @WebMvcTest(OrdinationControllerTest.class)
 public class OrdinationControllerTest {
+	
+	/*************************************
+	 * TEST CONTROLLER PER L'INVIO COMANDA
+	*************************************/
+	
 	@Autowired
 	private MockMvc mockMvc;
 
@@ -36,10 +48,6 @@ public class OrdinationControllerTest {
 	private Ordination ordination;
 	
 	ObjectMapper objectMapper = new ObjectMapper();
-
-	/*************************************
-	 * TEST CONTROLLER PER L'INVIO COMANDA
-	*************************************/
 	
 	@BeforeEach
 	void setup() {
@@ -61,19 +69,21 @@ public class OrdinationControllerTest {
 		/* 
 		 * setto l'ordinazione
 		*/
+		ordination.setUuid("eewfwe-efwffwf-2232sa-fdsfsdf");
 		ordination.setCreatedBy(user);
 		ordination.setTable(new Tables(1, true, "U2uu3-hsdbjhde-ksadadasd-dak2223"));
-		// non dovrebbe servire ordination.setDateCreation(new Date());
-		// non dovrebbe servire ordination.setNumbersOfCocktails(1);
-		// non dovrebbe servire Set<OrderedCocktail> ordsCocktails = new HashSet<OrderedCocktail>();
-		// Cocktail cocktail = new Cocktail("Negroni", 5.5, "Descrizione Negroni", "Amaro", true, true, true, 4);
-		// OrderedCocktail ordCocktail = new OrderedCocktail();
-		// ordCocktail.setCocktail(cocktail);
-		// ordCocktail.setOrdination(ordination);
-		// ordCocktail.setQuantity(1);
-		// ordsCocktails.add(ordCocktail);
-		// ordination.setOrderedCocktails(ordsCocktails);
-		// ordination.setStatus(OrdinationStatusEnum.CREATED);
+		ordination.setDateCreation(new Date());
+		ordination.setNumbersOfCocktails(1);
+		Set<OrderedCocktail> ordsCocktails = new HashSet<OrderedCocktail>();
+		Cocktail cocktail = new Cocktail("Negroni", 5.5, "Descrizione Negroni", "Amaro", true, true, true, 4);
+		OrderedCocktail ordCocktail = new OrderedCocktail();
+		ordCocktail.setCocktail(cocktail);
+		ordCocktail.setOrdination(ordination);
+	    ordCocktail.setQuantity(1);
+		ordsCocktails.add(ordCocktail);
+		ordination.setOrderedCocktails(ordsCocktails);
+		ordination.setStatus(OrdinationStatusEnum.CREATED);
+		
 		
 		//definsco ciò che passo alla created
 		// String cocktailUUID = "cock2-uuido22-peropriv-sjsj";
@@ -81,14 +91,16 @@ public class OrdinationControllerTest {
 		// cocktailsUuidList.add(cocktailUUID);
 		// OrdinationDTO ordinationDTO = new OrdinationDTO(cocktailsUuidList, "U2uu3-hsdbjhde-ksadadasd-dak2223", OrdinationStatusEnum.CREATED, new Date(), new Date(), "uuid-perordinati-snsn-2383", 5.5, user.getUuid());
 		OrdinationDTO ordinationDTO = new OrdinationDTO("U2uu3-hsdbjhde-ksadadasd-dak2223", user.getUuid());
+		ordinationDTO.uuid = "eewfwe-efwffwf-2232sa-fdsfsdf";
+		OrderedCocktailDTO orderedCocktailDTO = new OrderedCocktailDTO(cocktail.getUuid(), "eewfwe-efwffwf-2232sa-fdsfsdf"); 
 		
-		Mockito.when(ordinationService.create(Mockito.any(OrdinationDTO.class)))
+		Mockito.when(ordinationService.addCocktail(Mockito.any(OrderedCocktailDTO.class)))
 		.thenReturn(ordination);
 		
 		//definisco cio che passo in chiamata api mcvresult
-		String json = objectMapper.writeValueAsString(ordinationDTO);
+		String json = objectMapper.writeValueAsString(orderedCocktailDTO);
 		
-		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/manage-ordinations/create")
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/manage-ordinations/addCocktail")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 				//.andDo(MockMvcResultHandlers.print())
