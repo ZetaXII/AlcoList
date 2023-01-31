8afb2ee9551076574a988824f203da25a28de46e
dizenzoc <federicodizenzo91@gmail.com>
2023-01-31 22:33:50 +0100

    change mail sender in email service

--- AL-backend/src/main/java/it/fm3/alcolist/service/EmailService.java
+++ AL-backend/src/main/java/it/fm3/alcolist/service/EmailService.java
@@ -1,6 +1,7 @@
 package it.fm3.alcolist.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,14 @@ public class EmailService implements EmailServiceI {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    	private String emailAlcolist;
+    
     public void sendSimpleMessage(String to, String subject, String text) {
 
         SimpleMailMessage message = new SimpleMailMessage(); 
         //message.setFrom("noreply@alcolist.com");
-        message.setFrom("alcolistsrl@hotmail.com");
+        message.setFrom(emailAlcolist);
         message.setTo(to); 
         message.setSubject(subject); 
         message.setText(text);
